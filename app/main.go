f12271c225b1df9eb2cf0c5bcdd6f14e97b8f119
Bulanov Konstantin <leech001@gmail.com>
2022-06-18 20:58:13 +0300

    Update code: (#1)
    - delete function to only register the metric;
    - optimize func updateMetric;
    - added to Dockerfile 'go get ...'

--- app/main.go
+++ app/main.go
@@ -28,7 +28,7 @@ func main() {
 
 	// Cycle to create metrics from application parameters
 	for _, pName := range os.Args[5:] {
-		go setMetric(*metricUpdatePeriod, pName, regMetric(pName))
+		go updateMetric(*metricUpdatePeriod, pName)
 	}
 
 	// Running a web server to give back metrics
@@ -36,25 +36,19 @@ func main() {
 	http.ListenAndServe(":"+*metricPort, nil)
 }
 
-// Function to register the metric
-func regMetric(pName string) prometheus.Gauge {
-	metricName := pName + "_number_proccess"
-	return promauto.NewGauge(prometheus.GaugeOpts{
-		Name:        metricName,
+// Function to register and update the metric
+func updateMetric(updateTime int64, pName string) {
+	metric := promauto.NewGauge(prometheus.GaugeOpts{
+		Name:        pName + "_number_proccess",
 		Help:        "Number of processes running",
 		ConstLabels: prometheus.Labels{"proccess_name": pName},
 	})
-
-}
-
-// Function to register the metric
-func setMetric(updateTime int64, pName string, metric prometheus.Gauge) {
+	command := "ps waux | grep " + pName + "|grep -v -e 'grep'| grep -v -e '" + os.Args[0] + "'|wc -l"
 	for {
-		proc := "ps waux | grep " + pName + "|grep -v -e 'grep'| grep -v -e '" + os.Args[0] + "'|wc -l"
-		result, _ := exec.Command("bash", "-c", proc).Output()
-		metricStr := strings.ReplaceAll(strings.ReplaceAll(string(result), " ", ""), "\n", "")
-		metricFloat, _ := strconv.ParseFloat(string(metricStr), 64)
-		metric.Set(metricFloat)
+		result, _ := exec.Command("bash", "-c", command).Output()
+		valueStr := strings.ReplaceAll(strings.ReplaceAll(string(result), " ", ""), "\n", "")
+		valueFloat, _ := strconv.ParseFloat(string(valueStr), 64)
+		metric.Set(valueFloat)
 		time.Sleep(time.Duration(updateTime) * time.Second)
 	}
 }
